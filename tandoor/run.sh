52a85dd7c844d2fcb6efc7d318d50d82c7fd77f6
Kodey Converse <kodey@conve.rs>
2024-12-08 18:19:10 -0500

    Try fixing CSRF error

--- tandoor/run.sh
+++ tandoor/run.sh
@@ -10,6 +10,8 @@ export SECRET_KEY_FILE=/data/secret.txt
 export DB_ENGINE=django.db.backends.sqlite3
 export POSTGRES_DB=/data/recipes.db
 
+export ALLOWED_HOSTS=*
+
 export SORT_TREE_BY_NAME=0
 export ENABLE_PDF_EXPORT=1
 
